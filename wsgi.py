02e0e9c62c808c00b0e232053db8f302c42d067d
eoin <mcinerney.e@gmail.com>
2018-11-18 23:30:55 +0000

    remote push test

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["<h1 style='color:blue'>Hello There!</h1>"]
+    return ["Round Two!"]
 
