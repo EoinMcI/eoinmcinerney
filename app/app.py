3f638b0ee8b7d15c39fafb369eae50ead3a3155c
eoin <mcinerney.e@gmail.com>
2018-11-18 17:50:32 +0000

    basic test page

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, render_template, request, session, abort, flash, url_for
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "somethingsecure"
+
+
+@app.route("/")
+def home_pg():
+
+
+
+    return render_template('home.html',title='Eoin McInerney')
+
