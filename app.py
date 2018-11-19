a939124d97b83ec7d59261fdaec3b6cdc5eabef2
eoin <mcinerney.e@gmail.com>
2018-11-19 23:06:33 +0000

    added routing for individual articles

--- app.py
+++ app.py
@@ -1,30 +1,67 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, session, flash, url_for
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "somethingsecure"
+
 
 @app.route("/")
 def home_pg():
-    return "Home"
+
+	return render_template('home.html',title='Home')
 
 @app.route("/why")
 def why_pg():
-    return "Why"
+
+	return render_template('home.html',title='Why')
 
 @app.route("/about")
 def about_pg():
-    return "About"
+
+	return render_template('home.html',title='About')
+
+####################
 
 @app.route("/monthly-updates")
 def monthly_update_home_pg():
-    return "Monthly Updates"
-	
+
+	return render_template('home.html',title='Monthly Updates')
+
+@app.route("/monthly-updates/<string:article_name>/")
+def monthly_update_pg(article_name):
+    return article_name
+
+####################
+
 @app.route("/evergreen")
 def evergreen_home_pg():
-    return "Evergreens"
-	
+
+	return render_template('home.html',title='Evergreen')
+
+@app.route("/evergreen/<string:article_name>/")
+def evergreen_pg(article_name):
+    return article_name
+
+####################
+
 @app.route("/local")
 def local_home_pg():
-    return "Local"
-	
+
+	return render_template('home.html',title='Local')
+
+@app.route("/local/<string:article_name>/")
+def local_pg(article_name):
+    return article_name
+
+####################
+
 @app.route("/thoughts")
 def thoughts_home_pg():
-    return "Thoughts"
+
+	return render_template('home.html',title='Thoughts')
+	
+@app.route("/thoughts/<string:article_name>/")
+def thought_pg(article_name):
+    return article_name
+	
+	
