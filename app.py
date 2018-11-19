4a943359ca79c1de950271809f06842fbb4840be
eoin <mcinerney.e@gmail.com>
2018-11-19 18:48:03 +0000

    push to remote test

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ application.config['SECRET_KEY'] = "somethingsecure"
 @application.route("/")
 def home_pg():
 
-
-
     return render_template('home.html',title='Eoin McInerney')
 
 if __name__ == '__main__':
