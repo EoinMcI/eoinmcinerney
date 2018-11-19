c48221fad705184db7d780e174b0cb94474f0908
eoin <mcinerney.e@gmail.com>
2018-11-19 19:56:08 +0000

    push to remote test

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,14 @@
+from flask import Flask, redirect, render_template, request, session, abort, flash, url_for
 
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Round 6!"]
+application = Flask(__name__)
+application.config['SECRET_KEY'] = "somethingsecure"
 
+
+@application.route("/")
+def home_pg():
+
+    return render_template('home.html',title='Eoin McInerney')
+
+if __name__ == '__main__':
+    application.run()
